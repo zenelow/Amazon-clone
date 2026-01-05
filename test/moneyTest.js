e308c3b8cae88dde456e00771a87a904f59ce1d0
zenelow <ducminhsb2@gmail.com>
2026-01-05 16:00:35 +0700

    Create automated tests for formatCurrency

---
+++ test/moneyTest.js
@@ -0,0 +1,30 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
+console.log("Test suite: formatCurrency")
+
+console.log("Converts cents into dollars");
+
+if (formatCurrency(2095) === '20.95') {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
+
+console.log("Works with 0");
+
+if (formatCurrency(0) === '0.00') {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
+
+console.log("Rounds up to the nearest cent");
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log("Passed");
+} else {
+  console.log("Failed");
+}
+
+
+
