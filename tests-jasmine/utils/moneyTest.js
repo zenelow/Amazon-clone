ada3985b6dd6cc2e09de9e4411e266a5a69c262b
zenelow <ducminhsb2@gmail.com>
2026-01-06 21:48:44 +0700

    Created automated tests for addToCart

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 
 describe("Test suite: formatCurrency", () => {
   it("Converts cents into dollars", () => {
