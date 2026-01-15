17df8b07a6da02acaf7bbebe3c07d1d732d1ffab
zenelow <ducminhsb2@gmail.com>
2026-01-15 08:42:05 +0700

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -57,6 +57,39 @@ class Clothing extends Product {
 }
 
 
+/*
+console.log(this);
+
+const object2 = {
+  a: 2, 
+  b: this.a
+};
+*/
+
+/*
+const date = new Date(); 
+console.log(date);
+console.log(date.toLocaleTimeString());
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+
+logThis();
+logThis.call("hello");
+
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+}
+
+object3.method();
+*/
+
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
